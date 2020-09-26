0fd4c2b88e8f5f7b8684c1b1c103a0776628220e
peparhugo <peparhugo@gmail.com>
2020-09-26 17:05:10 -0600

    feat: linear regression for JPM ~ SPX

---
+++ submission_1/linear_regression/Linear_Regression_JPM_SPX.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 17:59:57 2020
+
+@author: Hamid
+"""
+
+from sklearn import linear_model
+import yfinance as yf
+
+# Download stock data for JP Morgan and the S&P 500
+jpm=yf.download("JPM", start="2018-02-01", end="2018-12-30")
+spx=yf.download("^GSPC", start="2018-02-01", end="2018-12-30")
+
+# 
+X = spx.iloc[:, spx.columns == 'Adj Close']
+Y = jpm['Adj Close']
+
+# Perform Linear Regression on the data
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+# Get values for Linear Regression Equation
+print('slope:', regr.coef_)
+print('intercept:', regr.intercept_)
