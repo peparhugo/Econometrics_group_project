8e3e4c8494856e671a933fa40c085a4603d0ecc5
peparhugo <peparhugo@gmail.com>
2020-09-27 17:25:31 -0600

    chore: added inline text describing what the cell block is doing

--- submission_1/basic_stats/basic_stats.py
+++ submission_1/basic_stats/basic_stats.py
@@ -4,32 +4,30 @@
 # In[1]:
 
 
+#import libraries
 import pandas as pd
-import numpy as np 
+import numpy as np
 import plotly.express as px
 
 
 # In[2]:
 
 
+#read in data
 df= pd.read_csv('JPM.csv')
-
-
-# In[3]:
-
-
-# df.set_index('Date',inplace=True)
 
 
 # In[4]:
 
 
+#view first 5 rows of JPM data
 df.head()
 
 
 # In[5]:
 
 
+#plot the adjusted close field overtime
 fig = px.line(df, x=df['Date'], y=df['Adj Close'])
 fig.show()
 
@@ -37,18 +35,21 @@ fig.show()
 # In[6]:
 
 
+#output summary of data distribution
 df['Adj Close'].describe(percentiles=[0.05,0.1,0.25,.33,0.5,0.67,0.75,0.9,0.95,0.99])
 
 
 # In[7]:
 
 
+#calcalate mean stock value
 df['mean_stock_value']=df['Adj Close'].mean(0)
 
 
 # In[8]:
 
 
+#calculate daily stock return using log returns and plot daily return data
 df['Daily Return Log'] = np.log(df['Adj Close']/(df['Adj Close'].shift(1)-1))
 fig = px.line(df, x=df['Date'], y=df['Daily Return Log'])
 fig.show()
@@ -57,6 +58,7 @@ fig.show()
 # In[9]:
 
 
+#calculate and plot the daily return percentage
 df['Daily Return Percent']=df['Daily Return Log']*100
 fig = px.line(df, x=df['Date'], y=df['Daily Return Percent'])
 fig.show()
@@ -65,18 +67,21 @@ fig.show()
 # In[10]:
 
 
+#display first 5 and last 5 rows of data
 df
 
 
 # In[11]:
 
 
+#calculate volatility
 volatility=df['Daily Return Log'].std()*100
 
 
 # In[12]:
 
 
+#output volatility
 volatility
 
 
@@ -97,6 +102,14 @@ fig.show()
 
 
 df[['Volatility_2nd']].describe()
+
+
+# In[17]:
+
+
+#export to other file formats
+get_ipython().system('jupyter nbconvert --to html "basic_stats.ipynb"')
+get_ipython().system('jupyter nbconvert --to python "basic_stats.ipynb"')
 
 
 # In[ ]:
